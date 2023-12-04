1b7def59002717de070b867d3eb02da368e3a24c
Th3VaultDweller <aztec242@yandex.ru>
2023-12-04 20:39:06 +0300

    Added symbol replacer just in case

--- wizarding_world_parser.py
+++ wizarding_world_parser.py
@@ -5,10 +5,10 @@ from bs4 import BeautifulSoup
 
 # url = "https://www.wizardingworld.com/discover/books"
 
-# headers = {
-#     "Accept": "*/*",
-#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-# }
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+}
 
 # req = requests.get(url, headers=headers)
 
@@ -19,21 +19,30 @@ with open("index.html") as file:
 
 soup = BeautifulSoup(src, "lxml")
 
-# находим все на звания книг и ссылки на них
-all_books_links = soup.find_all(class_="ProductCard_link__z-ZoA")
+# # находим все на звания книг и ссылки на них
+# all_books_links = soup.find_all(class_="ProductCard_link__z-ZoA")
 
-all_books_links_dict = {}
-for i, link in enumerate(all_books_links):
-    link_text = link.text  # название книги
-    link_href = link.get("href")  # ccылка на книгу
-    print(i)  # нумерация книг начинается с нуля
-    print(f"{link_text.strip()}: {link_href}")
+# all_books_links_dict = {}
+# for i, link in enumerate(all_books_links):
+#     link_text = link.text  # название книги
+#     link_href = link.get("href")  # ccылка на книгу
+#     print(i)  # нумерация книг начинается с нуля
+#     print(f"{link_text.strip()}: {link_href}")
 
-    # ключ - название книги, значение - ссылка на книгу
-    all_books_links_dict[link_text.strip()] = link_href
+#     # ключ - название книги, значение - ссылка на книгу
+#     all_books_links_dict[link_text.strip()] = link_href
 
-# сохраняем полученные названия и ссылки в словарь.json
-# indent=4 - необходимый отступ
-# ensure_ascii=False не экранирует символы и помогает в работе с кириллицей
-with open("all_books_links_dict.json", "w") as file:
-    json.dump(all_books_links_dict, file, indent=4, ensure_ascii=False)
+# # сохраняем полученные названия и ссылки в словарь.json
+# # indent=4 - необходимый отступ
+# # ensure_ascii=False не экранирует символы и помогает в работе с кириллицей
+# with open("all_books_links_dict.json", "w") as file:
+#     json.dump(all_books_links_dict, file, indent=4, ensure_ascii=False)
+
+with open("all_books_links_dict.json") as file:
+    all_books = json.load(file)
+
+for book_name, book_href in all_books.items():
+    rep = [",", ", ", "-", "'", ":"]
+    for item in rep:
+        if item in book_name:
+            book_name = book_name.replace(item, "_")
