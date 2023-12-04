7bc5450c40c183c4ed5f1a9de0a803422e4ac4c2
Th3VaultDweller <aztec242@yandex.ru>
2023-12-04 19:28:57 +0300

    Minor tweaks

--- wizarding_world_parser.py
+++ wizarding_world_parser.py
@@ -1,15 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.wizardingworld.com/discover/books"
+# url = "https://www.wizardingworld.com/discover/books"
 
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-}
+# headers = {
+#     "Accept": "*/*",
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+# }
 
-req = requests.get(url, headers=headers)
+# req = requests.get(url, headers=headers)
 
-src = req.text
+# src = req.text
 
-soup = BeautifulSoup(url, "lxml")
+with open("index.html") as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, "lxml")
